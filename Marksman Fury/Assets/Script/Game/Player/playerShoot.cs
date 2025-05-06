d7a2f49585d21d6e2980201352844d7afd3583bb
Erfan Khan <erfan.khan.cse@gmail.com>
2025-05-06 15:28:34 +0600

    Destroying Bullet when it hits Enemy

--- Marksman Fury/Assets/Script/Game/Player/playerShoot.cs
+++ Marksman Fury/Assets/Script/Game/Player/playerShoot.cs
@@ -5,24 +5,37 @@ public class playerShoot : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed;
+    [SerializeField] private GameObject gunOffset;
+    
+    [SerializeField] private float timeBetweenShots;
+    private float _lastFireTime;
 
     private bool _fireContinously;
+
+    private bool _fireSingle;
     
     
 
     // Update is called once per frame
     void Update()
     {
-        if (_fireContinously)
+        
+        if (_fireContinously || _fireSingle)
         {
-            FireBullet();
+            float timeSinceLastShot = Time.time - _lastFireTime;
+            if (timeSinceLastShot >= timeBetweenShots)
+            {
+                FireBullet();
+                _lastFireTime = Time.time;
+                _fireSingle = false;
+            } 
         }
         
     }
 
     private void FireBullet()
     {
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, gunOffset.transform.position, transform.rotation);
         Rigidbody2D rigidBody = bullet.GetComponent<Rigidbody2D>();
 
         rigidBody.linearVelocity = bulletSpeed * transform.up;
@@ -30,8 +43,22 @@ public class playerShoot : MonoBehaviour
 
     }
 
-    private void OnFire(InputValue inputValue)
+    public void OnAttack(InputValue inputValue)
     {
+        Debug.Log("Fire");
         _fireContinously = inputValue.isPressed;
     }
+    
+    
+    public void OnAttack1(InputValue inputValue)
+    {
+        _fireContinously = inputValue.isPressed;
+        if (inputValue.isPressed)
+        {
+            _fireSingle = true;
+        }
+        
+    }
+    
+    
 }
