096c52ff221251903bfea84f7fee9f4107528fde
Erfan Khan <erfan.khan.cse@gmail.com>
2025-05-13 02:16:41 +0600

    Displaying Score

---
+++ Marksman Fury/Assets/Script/Game/Score/ScoreController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoreController : MonoBehaviour
+{
+    public UnityEvent onScoreChanged;
+    
+    public int Score { get; private set; }
+
+    public void AddScore(int amount)
+    {
+        Score += amount;
+        onScoreChanged.Invoke();
+    }
+    
+}
